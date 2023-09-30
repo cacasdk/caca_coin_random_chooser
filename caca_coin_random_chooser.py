0873d43cf645c43b11598748c1d50ce58dbcdf9e
carrot7291 <hnf729@outlook.com>
2023-09-30 21:30:17 +0800

    initial commit

---
+++ caca_coin_random_chooser.py
@@ -0,0 +1,35 @@
+import json
+
+
+class CoinRandomChooser5:
+    def __init__(self, roster_file_path, coin_list):
+        self.roster_file_path = roster_file_path
+        self.coin_list = coin_list
+
+    @property
+    def make_student_number(self):
+        output: int = 0
+        for i in range(0, 5):
+            if self.coin_list[i] == '1':
+                output += 2 ** (i + 1)
+        return output
+
+    @property
+    def read_roster(self):
+        read = open(self.roster_file_path, "r")
+        output = json.loads(read.read())
+        return output
+
+    def chooser(self):
+        output = self.read_roster[self.make_student_number]
+        return output
+
+
+class CoinRandomChooser6(CoinRandomChooser5):
+    @property
+    def make_student_number(self):
+        output: int = 0
+        for i in range(0, 6):
+            if self.coin_list[i] == 1:
+                output += 2 ** (i + 1)
+        return output
